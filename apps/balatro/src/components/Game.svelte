990d322607a0bc2f21c93a6187831c23f72227c2
Daniel Ferreira Selga <danielselga@gmail.com>
2025-09-22 17:51:28 -0400

    comment tests

--- apps/balatro/src/components/Game.svelte
+++ apps/balatro/src/components/Game.svelte
@@ -25,7 +25,7 @@
 	import FreeSpinCounter from './FreeSpinCounter.svelte';
 	import FreeSpinOutro from './FreeSpinOutro.svelte';
 	import Transition from './Transition.svelte';
-	import I18nTest from './I18nTest.svelte';
+	// import I18nTest from './I18nTest.svelte';
 	import GameDock from './GameDock.svelte';
 
 	const context = getContext();
@@ -93,7 +93,7 @@
 		<FreeSpinOutro />
 		<Transition />
 
-		<I18nTest />
+		<!-- <I18nTest /> -->
 	{/if}
 </App>
 
